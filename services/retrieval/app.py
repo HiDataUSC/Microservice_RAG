e26651eb4bb766737331079869d46f0514b87eee
Dr-Banana <zhangxuanzhi10419@gmail.com>
2024-10-24 15:34:24 -0700

    create a pseudo main program. 1. can upload document to cloud/vectroized database 2. can retrieve the most relevant document to user

--- services/retrieval/app.py
+++ services/retrieval/app.py
@@ -7,7 +7,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 class Retriever:
     """Class to handle document retrieval from local vector store and downloading full documents from S3."""
-    def __init__(self, local_folder):
+    def __init__(self, local_folder = LOCAL_FOLDER):
         """Initialize the Retrieve class with a local folder for persistence of vector store data."""
         self.local_folder = local_folder
         self.redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
@@ -31,8 +31,11 @@ class Retriever:
         :param query: The query string to search for similar documents in the vector store.
         :return: The top similar document(s) based on the query.
         """
-        query_id = self.store_query_in_redis(query)
-        print(f"Query stored in Redis with ID: {query_id}")
+        try:
+            query_id = self.store_query_in_redis(query)
+            print(f"Query stored in Redis with ID: {query_id}")
+        except Exception:
+            pass
 
         vectorstore = Chroma(
             collection_name="summaries", 
@@ -53,13 +56,13 @@ class Retriever:
         s3_handler.download_file(doc_id, dst_folder)
 
     
-if __name__ == "__main__":
-    # Initialize Preprocessor
-    retriever = Retriever(LOCAL_FOLDER)
+# if __name__ == "__main__":
+#     # Initialize Preprocessor
+#     retriever = Retriever(LOCAL_FOLDER)
 
-    # store file with file_path
-    query = "Memory in agents"
-    for doc in retriever.retrieve(query):
-        doc_id = doc.metadata.get('doc_id')
-    dst_folder = r"E:\HiData\Microservice_RAG\tests"
-    retriever.full_document(doc_id, dst_folder)
+#     # store file with file_path
+#     query = "Memory in agents"
+#     for doc in retriever.retrieve(query):
+#         doc_id = doc.metadata.get('doc_id')
+#     dst_folder = r"E:\HiData\Microservice_RAG\tests"
+#     retriever.full_document(doc_id, dst_folder)
